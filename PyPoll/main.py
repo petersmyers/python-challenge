fb308d87c630716c3ba0ba33fd46c4e517ddac9c
PSMyers <petersmyers@wustl.edu>
2019-01-31 12:34:41 -0600

    the polls are printing

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -16,22 +16,22 @@ with open(csvpath, newline = '') as csvfile:
 
     # Count the number of rows as each row corresponds to a different month
     rowcount = sum(1 for row in csvvote)
-    
-    # Set total to zero so we have a baseline for accounting.
-    total = sum((int(row[1])) for row in csvvote)
-    
+
     # Get the names of the candidates
     candidates = [csvvote[0][2]]
     votes = [0]
     for row in csvvote:
-        for i in candidates:
-            if candidates[i]==row[2]:
-                votes[i] +- 1
-            else:
-                candidates [i+1]= row[2]
-                votes[i+1] = 1
-    winner = max(votes)
-
+        keep = 0
+        for i in range(0,len(candidates)):
+            if keep == 0:
+                if candidates[i]==row[2]:
+                    votes[i] += 1
+                    keep = 1
+        if keep == 0:    
+            candidates.append(row[2])
+            votes.append(int(1))
+    w = votes.index(max(votes))
+    winner = candidates[w]
     # Print the results
     # Bet Uncle Cleetus didn't do it this fast!
     print("The Election Results are in!")
@@ -39,17 +39,17 @@ with open(csvpath, newline = '') as csvfile:
     print(f"There were {rowcount} engaged citizens this election!")
     print("****************************")
     print(f"Here's the spread:")
-    for i in candidates:
-        print(f"{candidates[1]} received {(votes[1]/rowcount)*100}% of the votes ({votes[i]})")
+    for k in range(0,len(candidates)):
+        print(f"{candidates[k]} received {round((votes[k]/rowcount)*100, 2)}% of the votes ({votes[k]})")
     print("****************************")
     print(f"The winner of this election for an ambiguous office is...")
     print("** drum roll **")
-    print(f"{winner.upper}!!!!!")
+    print(f"{winner}!!!!!")
 
     # Now we have the PLEASURE of writing our results to a file.
-    out_csvpath = os.path.join('election_results.txt')
-    with open(out_csvpath, 'w', newline='') as txtfile:
-        txtwriter=txt.writer(txtfile, delimiter = ',')
-        txtwriter.writerow("Summary of the Profits and Losses")
+    # out_csvpath = os.path.join('election_results.txt')
+    # with open(out_csvpath, 'w', newline='') as txtfile:
+    #     txtwriter=txt.writer(txtfile, delimiter = ',')
+    #     txtwriter.writerow("Summary of the Profits and Losses")
 
 
